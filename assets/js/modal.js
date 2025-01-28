522e298f3343bc633c77eb77b9e5fa6eec4a756d
lipe-s-m <felipe.s.m1609@gmail.com>
2025-01-28 03:54:32 -0300

    feat: adjust layout modal and initial responsive

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -9,17 +9,37 @@ const backgroundModal = document.getElementById("background-modal");
 botaoHistorico.addEventListener("click", () => {
     backgroundModal.classList = "";
 
-    conversoes.map((conversao) => {
+    if (conversoes.length === 0) {
+        modal.insertBefore(document.createElement("hr"), modal.children[1])
+        return
+    }
+
+    conversoes.map((conversao, index) => {
         console.log(conversao);
         const elementoConversao = document.createElement("p");
-        elementoConversao.innerText = `${conversao.moedaOrigemA} ${Number(conversao.quantidadeMoedaA).toFixed(2)} x ${conversao.nomeMoedaB} = ${conversao.moedaDestinoB} ${conversao.resultadoConversao}`;
+        elementoConversao.innerText = `${conversao.moedaOrigemA} ${Number(conversao.quantidadeMoedaA).toFixed(2)}  x  ${conversao.nomeMoedaB} = ${conversao.moedaDestinoB} ${conversao.resultadoConversao}`;
+        elementoConversao.classList = 'conversao';
+        // .classList("separador-conversao")
+        const hr = document.createElement("hr");
+        hr.classList = "separador-conversao"
+        modal.insertBefore(hr, modal.children[1])
         modal.insertBefore(elementoConversao, modal.children[1]);
-
     })
-
 })
-botaoFecharHistorico.addEventListener("click", fecharHistorico)
-
+botaoFecharHistorico.addEventListener("click", () => {
+    if (conversoes.length === 0) {
+        document.getElementById("modal").querySelector("hr").remove();
+        fecharHistorico()
+        return
+    }
+    conversoes.map((conversao) => {
+        const conversaoNoDom = document.querySelector(".conversao");
+        console.log(conversaoNoDom.innerText);
+        conversaoNoDom.remove();
+        document.getElementById("modal").querySelector("hr").remove();
+    })
+    fecharHistorico()
+})
 
 
 function fecharHistorico() {
